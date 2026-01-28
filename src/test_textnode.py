aedf7aaf03c347fd2d52ad452183a56d26f61ebe
Frederic Poirier <uk_jobs@hotmail.com>
2026-01-28 08:45:03 -0500

    + fully defined isEqual and isNotEqual + adding unit tests for equal and notequal

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,31 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_notEq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_notEq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "www.google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.google.org")
+        self.assertNotEqual(node, node2)
+
+    def test_notEq_url_None(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.google.org")
+        self.assertNotEqual(node, node2)
+
+    def test_Eq_url_None(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, None)
+        self.assertEqual(node, node2)
+
+    def test_notEq_Type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.PLAIN)
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -17,7 +17,22 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
+        if self.text != other.text:
+            return False
+        if self.text_type != other.text_type:
+            return False
+        if self.url != other.url:
+            return False
         return True
+
+    def __ne__(self, other):
+        if self.text != other.text:
+            return True
+        if self.text_type != other.text_type:
+            return True
+        if self.url != other.url:
+            return True
+        return False
 
     def __repr__(self):  # TextNode(TEXT, TEXT_TYPE, URL)
         text_string = f"TextNode({self.text}, {self.text_type.value}, {self.url})"
